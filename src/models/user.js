00a5d5ead31194379c681ab8d53d7caa446cade8
Adil grover <adilgro@gmail.com>
2025-11-21 23:17:23 +0530

    git ignore added at right place

--- src/models/user.js
+++ src/models/user.js
@@ -16,7 +16,7 @@ const userSchema = mongoose.Schema({
         required:true,
         unique:true,
         trim:true,
-        maxLength :150,
+        maxLength :125,
         lowercase :true
     },
     password:{
